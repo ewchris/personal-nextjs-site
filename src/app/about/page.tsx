28d35103e9dad4e8758eddd6e74aeec3871c93a2
Evan C <ewchris101@gmail.com>
2024-05-11 21:47:35 -0400

    Added some components and pages... Will be the final commit for a while.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+
+export default function About() {
+  return (
+    <p>I'm the about page!</p>
+  );
+}
---
+++ src/components/Avatar.tsx
@@ -0,0 +1,101 @@
+export function AvatarXS() {
+  return (
+    <>
+      <span className="relative inline-block">
+        <img
+          className="h-6 w-6 rounded-full"
+          src="https://media.licdn.com/dms/image/C5603AQHdC_UjagF0Bw/profile-displayphoto-shrink_200_200/0/1565882702670?e=1721260800&v=beta&t=cPIY3GXlUXpFWfDvvQizwi_cmltkd7QMKKTAbhpLFKw"
+          alt=""
+        />
+        {/* Status indicator - Not included for now.
+        <span className="absolute bottom-0 right-0 block h-1.5 w-1.5 rounded-full bg-gray-300 ring-2 ring-white" /> 
+        */}
+      </span>
+    </>
+  )
+}
+
+export function AvatarS() {
+  return (
+    <>
+      <span className="relative inline-block">
+        <img
+          className="h-8 w-8 rounded-full"
+          src="https://media.licdn.com/dms/image/C5603AQHdC_UjagF0Bw/profile-displayphoto-shrink_200_200/0/1565882702670?e=1721260800&v=beta&t=cPIY3GXlUXpFWfDvvQizwi_cmltkd7QMKKTAbhpLFKw"
+          alt=""
+        />
+        {/* Status indicator - Not included for now.
+        <span className="absolute bottom-0 right-0 block h-2 w-2 rounded-full bg-red-400 ring-2 ring-white" /> 
+        */}
+      </span>
+    </>
+  )
+}
+
+export function AvatarM() {
+  return (
+    <>
+      <span className="relative inline-block">
+        <img
+          className="h-10 w-10 rounded-full"
+          src="https://media.licdn.com/dms/image/C5603AQHdC_UjagF0Bw/profile-displayphoto-shrink_200_200/0/1565882702670?e=1721260800&v=beta&t=cPIY3GXlUXpFWfDvvQizwi_cmltkd7QMKKTAbhpLFKw"
+          alt=""
+        />
+        {/* Status indicator - Not included for now.
+        <span className="absolute bottom-0 right-0 block h-2.5 w-2.5 rounded-full bg-green-400 ring-2 ring-white" /> 
+        */}
+      </span>
+    </>
+  )
+}
+
+export function AvatarL() {
+  return (
+    <>
+      <span className="relative inline-block">
+        <img
+          className="h-12 w-12 rounded-full"
+          src="https://media.licdn.com/dms/image/C5603AQHdC_UjagF0Bw/profile-displayphoto-shrink_200_200/0/1565882702670?e=1721260800&v=beta&t=cPIY3GXlUXpFWfDvvQizwi_cmltkd7QMKKTAbhpLFKw"
+          alt=""
+        />
+        {/* Status indicator - Not included for now.
+        <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full bg-gray-300 ring-2 ring-white" /> 
+        */}
+      </span>
+    </>
+  )
+}
+
+export function AvatarXL() {
+  return (
+    <>
+      <span className="relative inline-block">
+        <img
+          className="h-14 w-14 rounded-full"
+          src="https://media.licdn.com/dms/image/C5603AQHdC_UjagF0Bw/profile-displayphoto-shrink_200_200/0/1565882702670?e=1721260800&v=beta&t=cPIY3GXlUXpFWfDvvQizwi_cmltkd7QMKKTAbhpLFKw"
+          alt=""
+        />
+        {/* Status indicator - Not included for now.
+         <span className="absolute bottom-0 right-0 block h-3.5 w-3.5 rounded-full bg-red-400 ring-2 ring-white" /> 
+         */}
+      </span>
+    </>
+  )
+}
+
+export function AvatarXXL() {
+  return (
+    <>
+      <span className="relative inline-block">
+        <img
+          className="h-16 w-16 rounded-full"
+          src="https://media.licdn.com/dms/image/C5603AQHdC_UjagF0Bw/profile-displayphoto-shrink_200_200/0/1565882702670?e=1721260800&v=beta&t=cPIY3GXlUXpFWfDvvQizwi_cmltkd7QMKKTAbhpLFKw"
+          alt=""
+        />
+        {/* Status indicator - Not included for now.
+         <span className="absolute bottom-0 right-0 block h-4 w-4 rounded-full bg-green-400 ring-2 ring-white" /> 
+         */}
+      </span>
+    </>
+  )
+}
